0ff024ba3476e11c75c09796a7928e647d72b2cd
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-05-18 20:19:59 +0300

    Initial Commit

---
+++ StateAndDataFlowSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateAndDataFlowSwiftUI
+//
+//  Created by Евгения Шевцова on 18.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateAndDataFlowSwiftUI/StateAndDataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowSwiftUIApp.swift
+//  StateAndDataFlowSwiftUI
+//
+//  Created by Евгения Шевцова on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
